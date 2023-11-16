b33bf7185407effc456f1732fac38439ccd75eed
Lubou Kalbeka <kalbeka.lubou@gmail.com>
2023-11-16 18:51:34 +0300

    Add Lesson2

--- src/main/java/org/kalbeka/App.java
+++ src/main/java/org/kalbeka/App.java
@@ -8,6 +8,51 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Java!" );
+        printTheeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    private static void printTheeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    private static void checkSumSign() {
+        int a = 2;
+        int b = -2;
+        int sum = a+b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    private static void printColor() {
+        int value = 101;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        else if (value <= 100) {
+            System.out.println("Желтый");
+        }
+        else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    private static void compareNumbers() {
+        int a = 4;
+        int b = 7;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
     }
 }
