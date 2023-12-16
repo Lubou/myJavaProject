d53040f99346b0a52dcb106a57e07cd679343355
Lubou Kalbeka <kalbeka.lubou@gmail.com>
2023-12-17 02:33:09 +0300

    add Lesson11_Maven

--- src/main/java/org/kalbeka/App.java
+++ src/main/java/org/kalbeka/App.java
@@ -1,13 +1,9 @@
 package org.kalbeka;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Java!" );
+        System.out.println( "Hello Maven!" );
     }
 }
