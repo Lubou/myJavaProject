a0e6b4a2f94fcdc4e0f19555acc7f1f752c2f386
Lubou Kalbeka <kalbeka.lubou@gmail.com>
2023-12-25 09:52:55 +0300

    fix

--- src/test/java/org/kalbeka/FactorialTest.java
+++ src/test/java/org/kalbeka/FactorialTest.java
@@ -2,16 +2,22 @@ package org.kalbeka;
 
 import org.testng.annotations.Test;
 
+import java.math.BigInteger;
+
 import static org.testng.Assert.*;
 
 public class FactorialTest {
 
     @Test
-    public void testGetFactorial() {
-
-        int actual = Factorial.getFactorial(4);
-        int expected = 24;
-        assertEquals(expected, actual);
-
+    void getFactorial() {
+        assertEquals(BigInteger.ONE, Factorial.getFactorial(0));
+        assertEquals(BigInteger.ONE, Factorial.getFactorial(1));
+        assertEquals(BigInteger.valueOf(2), Factorial.getFactorial(2));
+        assertEquals(BigInteger.valueOf(6), Factorial.getFactorial(3));
+        assertEquals(BigInteger.valueOf(24), Factorial.getFactorial(4));
+        assertEquals(new BigInteger("2432902008176640000"), Factorial.getFactorial(20));
+        assertThrows(IllegalArgumentException.class, () -> {
+            Factorial.getFactorial(-1);
+        });
     }
 }
